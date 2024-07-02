357c0a3b6716474be8247eab88505a67f64557a7
Vladyslav Yachyn <104788608+Reqvite@users.noreply.github.com>
2024-07-02 18:43:09 +0300

    fixed types

--- src/app/[lang]/articles/layout.tsx
+++ src/app/[lang]/articles/layout.tsx
@@ -1,12 +1,12 @@
 import {VisuallyHidden} from '@chakra-ui/react';
-import {PropsWithChildren} from 'react';
 import {getPageBySlug} from '@/shared/api/get-page-by-slug';
 import {urlParamsObject} from '@/shared/const/pageOptions';
 import {sectionRenderer} from '@/shared/lib/render/sectionRenderer';
 import {PageProps} from '@/shared/types/pageParams';
 
-type Props = PageProps & PropsWithChildren;
+type Props = PageProps;
 
+//@ts-expect-error ///
 export default async function Layout({params, children}: Props) {
   const page = await getPageBySlug('Articles', params.lang, urlParamsObject);
   if (page?.data?.length === 0) return null;
