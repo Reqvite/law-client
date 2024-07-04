e74e0b309e3c73e8dab4317e8dadb0d8e49ea575
Vladyslav Yachyn <104788608+Reqvite@users.noreply.github.com>
2024-07-04 10:54:22 +0300

    updated animation

--- src/shared/const/animations.ts
+++ src/shared/const/animations.ts
@@ -1,10 +1,7 @@
 export const cardAnimation = {
   transition: 'transform 0.2s, box-shadow 0.2s',
   _hover: {
-    transform: 'translateY(0) scale(1.05)',
+    transform: 'translateY(0) scale(1.01)',
     boxShadow: 'lg'
-  },
-  _active: {
-    transform: 'translateY(0) scale(0.95)'
   }
 };
