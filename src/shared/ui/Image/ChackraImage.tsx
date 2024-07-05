af1cabb049b045ae99fc8276359cca8b16eee0c8
Vladyslav Yachyn <104788608+Reqvite@users.noreply.github.com>
2024-07-05 16:56:21 +0300

    test

--- src/shared/ui/Image/ChackraImage.tsx
+++ src/shared/ui/Image/ChackraImage.tsx
@@ -3,15 +3,8 @@ import {chakra} from '@chakra-ui/react';
 import NextImage from 'next/image';
 
 export default chakra(NextImage, {
-  shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt'].includes(prop)
+  shouldForwardProp: (prop) =>
+    ['width', 'height', 'src', 'alt', 'quality', 'placeholder', 'blurDataURL', 'loader'].includes(
+      prop
+    )
 });
-
-// 'use client';
-// import {chakra} from '@chakra-ui/react';
-// import NextImage from 'next/image';
-
-// export default chakra(NextImage, {
-//   shouldForwardProp: (prop) => 	['width', 'height', 'src', 'alt', 'quality', 'placeholder', 'blurDataURL', 'loader'].includes(
-// 			prop
-// 		).includes(prop)
-// });
