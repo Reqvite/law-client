9bd02da38683ad1feefd298fab0f4def12442d8b
Vladyslav Yachyn <104788608+Reqvite@users.noreply.github.com>
2024-07-05 15:42:25 +0300

    fixed types

--- src/app/[lang]/faculties/[faculty]/page.tsx
+++ src/app/[lang]/faculties/[faculty]/page.tsx
@@ -1,21 +1,5 @@
-import {Text} from '@chakra-ui/react';
-import {fetchArticles} from '@/shared/api/getArticles';
 import {Section} from '@/shared/ui';
 
 export default async function FacultyRoute({params}: {params: {faculty: string}}) {
-  const urlParamsObject = {
-    filters: {
-      slug: params.faculty
-    }
-  };
-  const {data} = await fetchArticles({urlParamsObject});
-
-  if (data.length === 0)
-    return (
-      <Section>
-        <Text>{params.faculty}</Text>
-      </Section>
-    );
-
   return <Section>{params.faculty}</Section>;
 }
